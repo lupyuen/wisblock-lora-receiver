9eebf42918138688ead010c147f91371e363ee96
lupyuen <luppy@appkaki.com>
2021-03-08 08:20:12 +0800

    Clean up

--- src/main.cpp
+++ src/main.cpp
@@ -55,64 +55,64 @@ static uint8_t RcvBuffer[64];
 void setup()
 {
 
-	// Initialize LoRa chip.
-	lora_rak4630_init();
+    // Initialize LoRa chip.
+    lora_rak4630_init();
 
-	// Initialize Serial for debug output
-	Serial.begin(115200);
-	while (!Serial)
-	{
-		delay(10);
-	}
-	Serial.println("=====================================");
-	Serial.println("LoRaP2P Rx Test");
-	Serial.println("=====================================");
+    // Initialize Serial for debug output
+    Serial.begin(115200);
+    while (!Serial)
+    {
+        delay(10);
+    }
+    Serial.println("=====================================");
+    Serial.println("LoRaP2P Rx Test");
+    Serial.println("=====================================");
 
-	// Initialize the Radio callbacks
-	RadioEvents.TxDone = NULL;
-	RadioEvents.RxDone = OnRxDone;
-	RadioEvents.TxTimeout = NULL;
-	RadioEvents.RxTimeout = OnRxTimeout;
-	RadioEvents.RxError = OnRxError;
-	RadioEvents.CadDone = NULL;
+    // Initialize the Radio callbacks
+    RadioEvents.TxDone = NULL;
+    RadioEvents.RxDone = OnRxDone;
+    RadioEvents.TxTimeout = NULL;
+    RadioEvents.RxTimeout = OnRxTimeout;
+    RadioEvents.RxError = OnRxError;
+    RadioEvents.CadDone = NULL;
 
-	// Initialize the Radio
-	Radio.Init(&RadioEvents);
+    // Initialize the Radio
+    Radio.Init(&RadioEvents);
 
-	// Set Radio channel
-	Radio.SetChannel(RF_FREQUENCY);
+    // Set Radio channel
+    Radio.SetChannel(RF_FREQUENCY);
 
-	// Set Radio RX configuration
-	Radio.SetRxConfig(
-		MODEM_LORA, 
-		LORA_BANDWIDTH, 
-		LORA_SPREADING_FACTOR,
-		LORA_CODINGRATE, 
-		0, 
-		LORA_PREAMBLE_LENGTH,
-		LORA_SYMBOL_TIMEOUT, 
-		LORA_FIX_LENGTH_PAYLOAD_ON,
-		0, 
-		true, 
-		0, 
-		0, 
-		LORA_IQ_INVERSION_ON, 
-		true
-	);
+    // Set Radio RX configuration
+    Radio.SetRxConfig(
+        MODEM_LORA, 
+        LORA_BANDWIDTH, 
+        LORA_SPREADING_FACTOR,
+        LORA_CODINGRATE, 
+        0, 
+        LORA_PREAMBLE_LENGTH,
+        LORA_SYMBOL_TIMEOUT, 
+        LORA_FIX_LENGTH_PAYLOAD_ON,
+        0, 
+        true, 
+        0, 
+        0, 
+        LORA_IQ_INVERSION_ON, 
+        true
+    );
 
-	// Start LoRa
-	Serial.println("Starting Radio.Rx");
-	Radio.Rx(RX_TIMEOUT_VALUE);
+    // Start LoRa
+    Serial.println("Starting Radio.Rx");
+    Radio.Rx(RX_TIMEOUT_VALUE);
 }
 
 void loop()
 {
-	// Handle Radio events
-	Radio.IrqProcess();
+    // Handle Radio events
+    Radio.IrqProcess();
 
-	// We are on FreeRTOS, give other tasks a chance to run
-	delay(100);
-	yield();
+    // We are on FreeRTOS, give other tasks a chance to run
+    delay(100);
+    yield();
 }
 
 /**@brief Function to be executed on Radio Rx Done event
@@ -120,37 +120,37 @@ void loop()
 void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr)
 {
     //  Show the timestamp in milliseconds
-  	Serial.print("OnRxDone: Timestamp=");
+    Serial.print("OnRxDone: Timestamp=");
     Serial.println(millis() / 1000);
-	delay(10);
-	memcpy(RcvBuffer, payload, size);
+    delay(10);
+    memcpy(RcvBuffer, payload, size);
 
     //  Show the signal strength, signal to noise ratio
-	Serial.printf("RssiValue=%d dBm, SnrValue=%d\n", rssi, snr);
+    Serial.printf("RssiValue=%d dBm, SnrValue=%d\n", rssi, snr);
 
     //  Show the packet received
-	for (int idx = 0; idx < size; idx++)
-	{
-		Serial.printf("%02X ", RcvBuffer[idx]);
-	}
-	Serial.println("");
+    for (int idx = 0; idx < size; idx++)
+    {
+        Serial.printf("%02X ", RcvBuffer[idx]);
+    }
+    Serial.println("");
 
     //  Receive the next packet
-	Radio.Rx(RX_TIMEOUT_VALUE);
+    Radio.Rx(RX_TIMEOUT_VALUE);
 }
 
 /**@brief Function to be executed on Radio Rx Timeout event
  */
 void OnRxTimeout(void)
 {
-	Serial.println("OnRxTimeout");
-	Radio.Rx(RX_TIMEOUT_VALUE);
+    Serial.println("OnRxTimeout");
+    Radio.Rx(RX_TIMEOUT_VALUE);
 }
 
 /**@brief Function to be executed on Radio Rx Error event
  */
 void OnRxError(void)
 {
-	Serial.println("OnRxError");
-	Radio.Rx(RX_TIMEOUT_VALUE);
+    Serial.println("OnRxError");
+    Radio.Rx(RX_TIMEOUT_VALUE);
 }
