4a0607ec9848ba8a8347449a1673df746be119d6
lupyuen <luppy@appkaki.com>
2021-03-09 17:52:17 +0800

    Clean up

--- src/main.cpp
+++ src/main.cpp
@@ -36,17 +36,17 @@ void OnRxError(void);
 // Define LoRa parameters. To receive LoRa packets from BL602, sync the parameters with
 // https://github.com/lupyuen/bl_iot_sdk/blob/lora/customer_app/sdk_app_lora/sdk_app_lora/demo.c#L41-L77
 // TODO: Change RF_FREQUENCY for your region
-#define RF_FREQUENCY 923000000	// Hz
-#define TX_OUTPUT_POWER 22		// dBm
-#define LORA_BANDWIDTH 0		// [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved]
-#define LORA_SPREADING_FACTOR 7 // [SF7..SF12]
-#define LORA_CODINGRATE 1		// [1: 4/5, 2: 4/6,  3: 4/7,  4: 4/8]
-#define LORA_PREAMBLE_LENGTH 8	// Same for Tx and Rx
-#define LORA_SYMBOL_TIMEOUT 0	// Symbols
+#define RF_FREQUENCY          923000000	// Hz
+#define TX_OUTPUT_POWER       22		// dBm
+#define LORA_BANDWIDTH        0		    // [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved]
+#define LORA_SPREADING_FACTOR 7         // [SF7..SF12]
+#define LORA_CODINGRATE       1		    // [1: 4/5, 2: 4/6,  3: 4/7,  4: 4/8]
+#define LORA_PREAMBLE_LENGTH  8	        // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT   0	        // Symbols
 #define LORA_FIX_LENGTH_PAYLOAD_ON false
-#define LORA_IQ_INVERSION_ON false
-#define RX_TIMEOUT_VALUE 3000
-#define TX_TIMEOUT_VALUE 3000
+#define LORA_IQ_INVERSION_ON       false
+#define RX_TIMEOUT_VALUE      3000
+#define TX_TIMEOUT_VALUE      3000
 
 static RadioEvents_t RadioEvents;
 
