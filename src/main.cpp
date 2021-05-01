a3a91009d29394ed67b31e2b1ea68ef09552c479
lupyuen <luppy@appkaki.com>
2021-05-01 10:03:14 +0800

    Test recive

--- src/main.cpp
+++ src/main.cpp
@@ -49,6 +49,33 @@ void OnRxError(void);
 #define RX_TIMEOUT_VALUE      3000
 #define TX_TIMEOUT_VALUE      3000
 
+///////////////////////////////////////////////////////////////////////////////
+//  Receive LoRaWAN Message
+
+//  Uncomment this to receive a LoRaWAN Join Network Request
+#define RECEIVE_LORAWAN_MESSAGE  ////
+
+#ifdef RECEIVE_LORAWAN_MESSAGE
+#define RF_FREQUENCY            923200000
+#define TX_OUTPUT_POWER                22         /* dBm */
+#define LORA_BANDWIDTH                  0         /* [0: 125 kHz, */
+                                                  /*  1: 250 kHz, */
+                                                  /*  2: 500 kHz, */
+                                                  /*  3: Reserved] */
+#define LORA_SPREADING_FACTOR          10         /* [SF7..SF12] */
+#define LORA_CODINGRATE                 1         /* [1: 4/5, */
+                                                  /*  2: 4/6, */
+                                                  /*  3: 4/7, */
+                                                  /*  4: 4/8] */
+#define LORA_PREAMBLE_LENGTH            8         /* Same for Tx and Rx */
+#define LORA_SYMBOL_TIMEOUT             5         /* Symbols */
+#define LORA_FIX_LENGTH_PAYLOAD_ON      false
+#define LORA_IQ_INVERSION_ON            false
+
+#define RX_TIMEOUT_VALUE                3000    /* ms */
+#define TX_TIMEOUT_VALUE                5000    /* ms */
+#endif  //  RECEIVE_LORAWAN_MESSAGE
+
 //  Callback Functions for LoRa Events
 static RadioEvents_t RadioEvents;
 
